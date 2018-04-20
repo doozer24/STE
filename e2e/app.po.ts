c4659eb562e03b30f0744dc3a37e75a0b6e25f72
Lee Kirkland <weeman.kirkland@gmail.com>
2018-04-20 17:34:39 -0400

    selenium is working as expected and protractor has been setup, to run selenium. To run with docker you must use this. ocker-compose -f docker-compose.yml -f selenium.yml up -d

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,11 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  static navigateTo() {
+  navigateTo() {
     return browser.get('/');
   }
 
-  static getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getParagraphText() {
+    return element(by.css('div')).getText();
   }
 }
