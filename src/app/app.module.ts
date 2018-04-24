8be05a76d29c2d7b08276186dd872d15d11d21cc
Alex Hoffman <alexhoffman617@gmail.com>
2018-04-24 10:08:32 -0400

    Initial setup for frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+const routes: Routes = [
+  {path: '', component: MainComponent, children:
+  [
+    {path: '', component: HomeComponent}
+  ]
+}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
