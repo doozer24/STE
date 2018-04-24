64afd857d59523ed1f05c72cc3f27ded3d8f2a14
Alex Hoffman <alexhoffman617@gmail.com>
2018-04-24 11:23:47 -0400

    App structure basic layout

---
+++ src/app/create-time-card/create-time-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-time-card',
+  templateUrl: './create-time-card.component.html',
+  styleUrls: ['./create-time-card.component.css']
+})
+export class CreateTimeCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
