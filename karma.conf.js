2e9f4a0532e13f6a2b1c809cd7b5647e4ad3ae80
John Thompson <john.h.thompson@uscis.dhs.gov>
2018-04-26 16:09:50 -0400

    trailing commas are nice

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
       dir: path.join(__dirname, 'coverage'),
     },
     junitReporter: {
