d8a96ce60414381c7508fe2adb3a92c3b14f0e31
Lee Kirkland <weeman.kirkland@gmail.com>
2018-04-29 17:03:15 -0400

    updated to add axe to protractor e2e test.

--- protractor.conf.js
+++ protractor.conf.js
@@ -17,13 +17,17 @@ exports.config = {
   directConnect: false,
   //needs to be ran on the selenium docker image, or if running in a docker compose setup with the image name of
   //the web app.
-  baseUrl: 'http://app:4200/',
+  baseUrl: 'http://a3ee2941848a611e885050aed1a33dd7-259618607.us-east-1.elb.amazonaws.com:8080/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
+  plugins: [{
+    axe: true,
+    package: 'protractor-accessibility-plugin'
+  }],
   onPrepare() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
