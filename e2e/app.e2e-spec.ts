32143e610d9d400e8fa97fe254017378850c705b
Gogs008 <gbehal@gmail.com>
2018-04-29 01:52:45 -0400

    updates to e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,17 +1,42 @@
-import { AppPage } from './app.po';
+import { LoginPage, LoggedInHome, CreateTimeCard} from './app.po';
 import {tryCatch} from 'rxjs/util/tryCatch';
 import { browser, by, element } from 'protractor';
 
-describe('sevis-challenge-frontend App', () => {
-  let page: AppPage;
+describe('Login page', () => {
+  let loginPage: LoginPage;
+  let home: LoggedInHome;
+  let createTimeCard: CreateTimeCard;
+
+  const invalidCredentials = {
+    username: 'invalidname',
+    password: 'invalidpassword'
+  };
 
   beforeEach(() => {
-    page = new AppPage();
+    loginPage = new LoginPage();
+    home = new LoggedInHome();
+    createTimeCard = new CreateTimeCard();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    //browser.pause();
-    expect(page.getParagraphText()).toEqual('Sevis Frontend Hello World',page.toString());
+  it('when the user trys to login with invalid credentials they should stay on “login page" and see error notification', () => {
+    loginPage.navigateTo();
+    loginPage.fillLoginFields(invalidCredentials);
+    expect(loginPage.getPageTitle()).toEqual('Log In');
+    expect(loginPage.getErrorMessage()).toEqual('There was an error logging in.');
   });
+
+  it('when login is successful the user should redirect to welcome page', () => {
+    loginPage.navigateTo();
+    loginPage.fillLoginFields();
+    expect(home.getWelcomeMessage()).toEqual('Welcome John Doe');
+  });
+
+  it('when the user clicks Logout button they should be logged out and redirected to login page', () => {
+    home.navigateTo();
+    home.logOut()
+    expect(loginPage.getPageTitle()).toEqual('Log In');
+  });
+
+
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,12 +1,75 @@
 import { browser, by, element } from 'protractor';
 
-export class AppPage {
-  navigateTo() {
+export class LoginPage {
+
+  private credentials = {
+    username: 'dummy',
+    password: 'pass123'
+  };
+
+  navigateTo(){
+    return browser.get('/login');
+  }
+
+  fillLoginFields(credentials: any = this.credentials) {
+    element(by.css('[id="username"]')).sendKeys(credentials.username);
+    element(by.css('[id="password"]')).sendKeys(credentials.password);
+    element(by.css('[id="login"]')).click();
+  }
+
+  getPageTitle(){
+    return element(by.css('legend')).getText();
+  }
+
+  getErrorMessage(){
+    return element(by.css('[role="alert"]')).getText();
+  }
+
+}
+
+export class LoggedInHome {
+  navigateTo(){
     return browser.get('/');
   }
 
-  getParagraphText() {
-    // return element(by.css('div')).getText();
-    return element(by.css('div')).getText();
+  logOut(){
+    return element(by.css('a[href="#"]')).click();
   }
+
+  getWelcomeMessage(){
+    return element(by.css('[class="page-subheader"]'));
+  }
+
+  createTimeCardButton(){
+    return element(by.css('[routerLink="/create-time-card"]')).click();
+  }
+
 }
+
+export class CreateTimeCard {
+  navigateTo(){
+    return browser.get('/create-time-card');
+  }
+
+  logOut(){
+    return element(by.css('a[href="#"]')).click();
+  }
+
+  getWelcomeMessage(){
+    return element(by.css('[class="page-subheader"]'));
+  }
+
+  cancelButton(){
+    return element(by.css('[class="usa-button-secondary"]'));
+  }
+
+}
+
+
+
+
+
+
+
+
+
