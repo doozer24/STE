b2b05949040d73a835c7e3970c099de747856403
Gogs008 <gbehal@gmail.com>
2018-04-30 09:22:30 -0400

    Increased-Time-to-Jasmine-timeout
    Increased-Time-to-Jasmine-timeout to 300,000 from 30,000

--- protractor.conf.js
+++ protractor.conf.js
@@ -21,7 +21,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 300000,
     print: function() {}
   },
   plugins: [{
