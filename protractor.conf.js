0b7c3f91b72328ecb7f9a2ba3b76394cf28b2def
Gogs008 <gbehal@gmail.com>
2018-04-30 14:58:45 -0400

    DSCTS-36 Bug Fix
    Made Logout button not show when user is not logged in

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,22 +12,20 @@ exports.config = {
     'browserName': 'chrome'
   },
   //for use with selenium
-  seleniumAddress: 'http://selenium:4444/wd/hub',
+  //seleniumAddress: 'http://selenium:4444/wd/hub',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
 
   directConnect: false,
   //needs to be ran on the selenium docker image, or if running in a docker compose setup with the image name of
   //the web app.
-  baseUrl: 'http://a3ee2941848a611e885050aed1a33dd7-259618607.us-east-1.elb.amazonaws.com:8080',
+  //baseUrl: 'http://app:4200/',
+  baseUrl: 'http://172.22.124.225:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 300000,
+    defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  plugins: [{
-      axe: true,
-      package: 'protractor-accessibility-plugin'
-  }],
   onPrepare() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
