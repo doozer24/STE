de74fd3d0bfe2dbdd14d2a959eb2944570adb752
Alex Hoffman <alexhoffman617@gmail.com>
2018-04-30 14:18:34 -0400

    Tests

--- src/app/time-card/time-card.component.spec.ts
+++ src/app/time-card/time-card.component.spec.ts
@@ -8,16 +8,17 @@ import { Component } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
+import { promise } from 'protractor';
+import { TimeCard, Time } from '../models/time-card';
 
 describe('TimeCardComponent', () => {
   let component: TimeCardComponent;
   let fixture: ComponentFixture<TimeCardComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TimeCardComponent ],
       imports: [RouterTestingModule, FormsModule, DialogModule, BrowserAnimationsModule, HttpModule],
-      providers: [TimeCardService, ProjectService]
+      providers: [{provide: TimeCardService, useClass: MockTimeCardService}, ProjectService]
     })
     .compileComponents();
   }));
@@ -31,11 +32,43 @@ describe('TimeCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('get dates gets right amount of dates from array', () => {
+    fixture = TestBed.createComponent(TimeCardComponent);
+    component = fixture.componentInstance;
+    const datesArray = component.getDates(new Date(2018, 1, 1), new Date(2018, 1, 7));
+    expect(datesArray.length).toEqual(7);
+  });
+
+  it('correctly identifies unique project tasks', () => {
+    fixture = TestBed.createComponent(TimeCardComponent);
+    component = fixture.componentInstance;
+    component.timeCard = new TimeCard(0, 0, new Date(), new Date(), '', [
+      new Time(new Date(), 0, 1, 1),
+      new Time(new Date(), 0, 1, 2),
+      new Time(new Date(), 0, 1, 2),
+      new Time(new Date(), 0, 2, 1),
+      new Time(new Date(), 0, 2, 1),
+    ]);
+    const uniqueProjectAndTask = component.getUniqueProjectAndTask();
+    expect(uniqueProjectAndTask.length).toEqual(3);
+  });
 });
 
-// @Component({
-//   selector: 'p-dialog',
-//   template: ''
-// })
-// class PDialogComponent {
-// }
+export class MockTimeCardService {
+  saveTimeCard() {
+    return new Promise(function(resolve) {
+      resolve(true);
+    });
+  }
+
+  deleteTimeCard() {
+    return true;
+  }
+
+  getTimeCard() {
+    return new Promise(function(resolve) {
+      return new TimeCard(1, 1, new Date(2017, 1, 1), new Date(2017, 1, 7), 'IN PROGRESS');
+    });
+  }
+}
