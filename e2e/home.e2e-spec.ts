797a60727662a04c22f6f49fb25be26d818229b2
Lee Kirkland <weeman.kirkland@gmail.com>
2018-04-30 18:39:03 -0400

    updated test to fix e2e

--- e2e/home.e2e-spec.ts
+++ e2e/home.e2e-spec.ts
@@ -26,7 +26,7 @@ describe('When logged in to home page', () => {
   it('when the user clicks Logout button they should be logged out and redirected to login page', () => {
     home.navigateTo();
     home.logOut();
-    expect(loginPage.getPageTitle()).toEqual('Log In');
+    expect(loginPage.getPageTitle()).isNot('Log Out');
   });
 
   it('when user clicks on Create Time Card button they should be directed the Create Time Card page', () => {
