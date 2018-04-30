14eb289d4bc4087dcddd1e6c10f6af7d1976afc9
Ci-Ci Thomson <public.thomson@gmail.com>
2018-04-30 09:27:42 -0400

    Update project.service.ts (#39)

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -6,7 +6,7 @@ import * as _ from 'lodash';
 @Injectable()
 export class ProjectService {
   //port = 'http://localhost:8090';
-  port = 'http://sevis-challenge-back-project:8080';
+  port = 'http://a4ae6d2124a2711e885050aed1a33dd7-1805500835.us-east-1.elb.amazonaws.com:8080';
 
   constructor(private http: Http) { }
 
