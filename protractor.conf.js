f31ed17e4fc5803f6dc2b0fb7186b3d5da39ffb2
Gogs008 <gbehal@gmail.com>
2018-04-30 09:00:38 -0400

    fixed merge conflicts
    fixed merge conflicts

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,18 +12,13 @@ exports.config = {
     'browserName': 'chrome'
   },
   //for use with selenium
-  //seleniumAddress: 'http://selenium:4444/wd/hub',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://selenium:4444/wd/hub',
 
   directConnect: false,
   //needs to be ran on the selenium docker image, or if running in a docker compose setup with the image name of
   //the web app.
-<<<<<<< HEAD
-  //baseUrl: 'http://app:4200/',
-  baseUrl: 'http://172.22.124.225:4200/',
-=======
+  baseUrl: 'http://app:4200/',
   baseUrl: 'http://a3ee2941848a611e885050aed1a33dd7-259618607.us-east-1.elb.amazonaws.com:8080',
->>>>>>> master
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
