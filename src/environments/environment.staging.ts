e68cd9a05b7bc77dc417c69710cce80413a90ba9
Gogs008 <gbehal@gmail.com>
2018-05-01 10:40:52 -0400

    Update environment.staging.ts

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,5 +4,3 @@ export const environment = {
   projectRoute: 'http://a4ae6d2124a2711e885050aed1a33dd7-1805500835.us-east-1.elb.amazonaws.com:8080/project',
   timeRoute: 'http://a51d659e34bf811e885050aed1a33dd7-1312644071.us-east-1.elb.amazonaws.com:8080/time'
 };
-
-//ng serve --env=staging
