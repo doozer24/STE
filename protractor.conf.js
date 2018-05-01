e326c188cf640e9c2764bf09377370dc6e589c01
Gogs008 <gbehal@gmail.com>
2018-05-01 10:40:05 -0400

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -11,14 +11,12 @@ exports.config = {
     'browserName': 'chrome'
   },
   //for use with selenium
-  //seleniumAddress: 'http://selenium:4444/wd/hub',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://selenium:4444/wd/hub',
 
   directConnect: false,
   //needs to be ran on the selenium docker image, or if running in a docker compose setup with the image name of
   //the web app.
-  //baseUrl: 'http://a3ee2941848a611e885050aed1a33dd7-259618607.us-east-1.elb.amazonaws.com:8080',
-  baseUrl: 'http://192.168.1.3:4200',
+  baseUrl: 'http://a3ee2941848a611e885050aed1a33dd7-259618607.us-east-1.elb.amazonaws.com:8080',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
